a0ada306da604b81c8238c297f6e86d0de5a8ba7
YiT-73 <yitzhaknamihas@gmail.com>
2024-10-13 18:20:32 -0500

    problema con mascotra respository

--- src/main/java/com/dbp/pet_journey/auth/infraestructure/UserAccountRepository.java
+++ src/main/java/com/dbp/pet_journey/auth/infraestructure/UserAccountRepository.java
@@ -2,6 +2,7 @@ package com.dbp.pet_journey.auth.infraestructure;
 
 import com.dbp.pet_journey.auth.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
