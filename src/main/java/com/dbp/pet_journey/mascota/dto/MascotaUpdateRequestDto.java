d26a10a850f2895e88b33b5c032c9f313f9702cd
YiT-73 <yitzhaknamihas@gmail.com>
2024-11-24 19:04:18 -0500

    actualizacion al máscota update dto

--- src/main/java/com/dbp/pet_journey/mascota/dto/MascotaUpdateRequestDto.java
+++ src/main/java/com/dbp/pet_journey/mascota/dto/MascotaUpdateRequestDto.java
@@ -1,17 +1,30 @@
 package com.dbp.pet_journey.mascota.dto;
 
+import com.dbp.pet_journey.mascota.domain.Especie;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class MascotaUpdateRequestDto {
+    @Size(min = 1, max = 50)
+    private String name;
+
+    @Size(min = 5, max = 60)
+    private String breed;
+
+    private Especie especie;
+
     @NotNull
     private Double weight;
 
     @Size(max = 200)
     @NotNull
     private String caracteristics;
+
+    private LocalDate fecha_nacimiento;
 }
